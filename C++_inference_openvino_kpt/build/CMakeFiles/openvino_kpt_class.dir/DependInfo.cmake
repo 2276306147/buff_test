dcffcfae66f5fedf466c4999e65017f341535dd9
baicheng <93041172+2276306147@users.noreply.github.com>
2023-10-27 19:32:44 +0800

    Add files via upload

---
+++ C++_inference_openvino_kpt/build/CMakeFiles/openvino_kpt_class.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bc/桌面/buff1/YOLO-of-RoboMaster-Keypoints-Detection-2023-main/C++_inference_openvino_kpt/demo_kpt.cpp" "CMakeFiles/openvino_kpt_class.dir/demo_kpt.cpp.o" "gcc" "CMakeFiles/openvino_kpt_class.dir/demo_kpt.cpp.o.d"
+  "/home/bc/桌面/buff1/YOLO-of-RoboMaster-Keypoints-Detection-2023-main/C++_inference_openvino_kpt/yolov7_kpt.cpp" "CMakeFiles/openvino_kpt_class.dir/yolov7_kpt.cpp.o" "gcc" "CMakeFiles/openvino_kpt_class.dir/yolov7_kpt.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
